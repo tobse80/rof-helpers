5c767482827101064b48c51d2e94f141ee9d079d
tobse80 <tobse.unique@gmail.com>
2020-04-01 01:33:12 +0200

    Fix ping tests

--- src/rof/ping.py
+++ src/rof/ping.py
@@ -11,10 +11,17 @@ def is_target_reachable(target:str, tmoSec:int=1) -> bool:
       return False
 
 def ping(target:str, tmoSec:int=1, count:int=1, size:int=64) -> CompletedProcess:
+   if tmoSec < 0: tmoSec = 0  # 0 means forever
+   if count < 1: count = 1
+   if size < 8: size = 8
+
    if platform.startswith('linux'):
       cmd = ['ping', '-W', str(tmoSec), '-c', str(count), '-s', str(size - 8), target]
    elif platform.startswith('win'):
-      cmd = ['ping', '-w', str(tmoSec * 1000), '-n', str(count), '-l', str(size), target]
+      if tmoSec == 0:
+         cmd = ['ping', '-t', '-n', str(count), '-l', str(size), target]
+      else:
+         cmd = ['ping', '-w', str(tmoSec * 1000), '-n', str(count), '-l', str(size), target]
    else:
       raise RuntimeError('"' + str(platform) + '" not supported.')
 
