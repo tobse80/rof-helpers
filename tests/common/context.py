a141e3f1ad5ac91514942ef0113c538b11863c5e
tobse80 <tobse.unique@gmail.com>
2020-04-15 21:43:28 +0200

    Refactor directory structure

--- tests/common/context.py
+++ tests/common/context.py
@@ -1,3 +1,3 @@
 # For testing a local package, import it via tests.context
 import tests.context
-import rof
+import common
--- tests/common/test_ping.py
+++ tests/common/test_ping.py
@@ -1,4 +1,4 @@
-from .context import rof
+from .context import common
 import pytest
 import subprocess
 
@@ -7,25 +7,25 @@ pytestmark = pytest.mark.timeout(1.2)
 
 def test_is_target_reachable__loopback():
    # should always succeed
-   assert rof.is_target_reachable("127.0.0.1")
+   assert common.is_target_reachable("127.0.0.1")
 
 def test_is_target_reachable__linklocal():
    # assumption: specific address is not present in current link-local net
    # PING: Fehler bei der Übertragung. Allgemeiner Fehler.
-   assert not rof.is_target_reachable("169.254.0.1")
+   assert not common.is_target_reachable("169.254.0.1")
 
 def test_is_target_reachable__private():
    # assumption: specific address is not present in this private net
    # Antwort von a.b.c.d: Zielnetz nicht erreichbar.
-   assert not rof.is_target_reachable("192.168.212.212")
+   assert not common.is_target_reachable("192.168.212.212")
 
 def test_is_target_reachable__notReachable():
    # assumption: specific address is not reachable
    # Zeitüberschreitung der Anforderung.
-   assert not rof.is_target_reachable("2.3.4.5")
+   assert not common.is_target_reachable("2.3.4.5")
 
 def test_is_target_reachable__timeoutExpired():
    with pytest.raises(subprocess.TimeoutExpired):
       # assumption: specific address is not reachable
       # Zeitüberschreitung der Anforderung.
-      rof.is_target_reachable("2.3.4.5", timeout=0)
+      common.is_target_reachable("2.3.4.5", timeout=0)
